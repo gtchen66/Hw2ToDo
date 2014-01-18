0aa13dea2077927b60838e6ce973c404efaaba08
gtchen66 <gtchen66@yahoo.com>
2014-01-17 22:37:29 -0800

    Initial Commit

---
+++ Hw2ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hw2ToDo
+//
+//  Created by George Chen on 1/17/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hw2ToDo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hw2ToDo
+//
+//  Created by George Chen on 1/17/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
